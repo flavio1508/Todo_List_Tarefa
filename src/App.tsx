574e4788e5eb41215d196c4269642af26d56fe08
flavio1508 <flavioaugusto1508@gmail.com>
2023-03-12 22:55:32 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,62 @@
-import React from 'react';
+import { type } from 'os';
+import React, { useRef, useState } from 'react';
 import logo from './logo.svg';
-import './App.css';
 
+type ITarefa = {
+  id: number;
+  tarefa: string;
+  pronto: boolean
+};
 function App() {
+  const [tarefas, setTarefas] = useState<ITarefa[]>([]);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  const criarTarefa = () => {
+    if (inputRef.current) {
+      setTarefas([
+        { id: Math.random(), tarefa: inputRef.current.value, pronto: false },
+        ...tarefas,
+      ]);
+      inputRef.current.value = "";
+    }
+  };
+  const tornarTarefaPronto=(id:number)=>{
+    setTarefas(
+      tarefas.map((tarefa)=>({
+        id:tarefa.id,
+        tarefa:tarefa.tarefa,
+        pronto:tarefa.id === id ? !tarefa.pronto : tarefa.pronto,
+      }))
+    )
+  };
+  const deletarTarefa = (id:number)=>{
+    setTarefas(tarefas.filter((tarefa)=> tarefa.id !==id))
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen bg-slate-700 flex items-center justify-center">
+      <div className='bg-slate-200 p-4 rounded-lg'>
+        <h1>Tarefa</h1>
+        {tarefas.length > 0 && (<div className='bg-slate-700 p-2 rounded-md flex flex-col gap-4'>
+          {tarefas.map((tarefa) =>
+            <div className='bg-slate-200 p-2 rounded-md flex justify-between gap-3' key={tarefa.id}>
+              <span className={tarefa.pronto?"line-through font-bold text-emerald-100": ""}>{tarefa.tarefa}</span>
+              <div>
+                <button onClick={()=> tornarTarefaPronto(tarefa.id)} className={tarefa.pronto ? "bg-emerald-800": "bg-slate-800"}>Pronto</button>
+                <button onClick={()=> deletarTarefa(tarefa.id)} className='bg-red-800 ml-3'>Deletar</button>
+              </div>
+            </div>)}
+        </div>)}
+
+        <form onSubmit={(e) => {
+          e.preventDefault();
+          criarTarefa();
+        }}>
+          <input ref={inputRef} type="text" required />
+          <button className='bg-emerald-800'>Criar tarefa</button>
+        </form>
+      </div>
     </div>
+
   );
 }
 
